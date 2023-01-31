af1c6473d42fb21c72e405caf33ff69301e8eac2
lothin <lothin5000000@gmail.com>
2023-02-01 01:45:11 +0700

    Add product section

--- src/main/java/com/lothin/phoneshp/exception/GlobleExceptionHandler.java
+++ src/main/java/com/lothin/phoneshp/exception/GlobleExceptionHandler.java
@@ -1,14 +1,24 @@
 package com.lothin.phoneshp.exception;
 
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobleExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+    }
+
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<?> handleExceptionClientError(ApiException e) {
-        ErrorResponse errorResponse = new ErrorResponse(e.getStatus().getReasonPhrase(),e.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(e.getStatus().getReasonPhrase(), e.getMessage());
         return ResponseEntity.status(e.getStatus()).body(errorResponse);
 
     }
---
+++ src/main/java/com/lothin/phoneshp/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.lothin.phoneshp.service;
+
+import com.lothin.phoneshp.model.Product;
+
+public interface ProductService {
+    Product saveProduct(Product product);
+    
+}
